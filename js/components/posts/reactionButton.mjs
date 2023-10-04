8af878d4bd8c442684995c8981f1e53c5689c0ae
Kim Rune Møller <kimrune@hotmail.no>
2023-10-04 13:46:09 +0200

    reaction button refactor

--- js/components/posts/reactionButton.mjs
+++ js/components/posts/reactionButton.mjs
@@ -1,4 +1,6 @@
-export default function reactionButton(reactions) {
+import reactionHandler from "../../handlers/reactionHandler.mjs";
+
+export default function reactionButton(postId, reactions) {
   const html = `
       <div class="dropdown">
       <button class="me-3 btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -11,21 +13,27 @@ export default function reactionButton(reactions) {
       </button>
       <ul class="dropdown-menu">
         <li class="dropdown-item">
-          <button class="btn">            
+          <button class="btn react-button">            
             <span class="ms-2">👍</span>
-            <span class="ms-2">1432</span>
+            <span class="ms-2">${
+              reactions.filter((reaction) => reaction.symbol === "👍").length
+            }</span>
           </button>
         </li>
         <li class="dropdown-item">
-          <button class="btn">            
+          <button class="btn react-button">            
             <span class="ms-2">❤️</span>
-            <span class="ms-2">543</span>
+            <span class="ms-2">${
+              reactions.filter((reaction) => reaction.symbol === "❤️").length
+            }</span>
           </button>
         </li>
         <li class="dropdown-item">
-          <button class="btn">            
+          <button class="btn react-button">            
             <span class="ms-2">😂</span>
-            <span class="ms-2">985</span>
+            <span class="ms-2">${
+              reactions.filter((reaction) => reaction.symbol === "😂").length
+            }</span>
           </button>
         </li>
       </ul>      
@@ -34,5 +42,38 @@ export default function reactionButton(reactions) {
   const template = document.createElement("template");
   template.innerHTML = html;
   const container = template.content.cloneNode(true);
+
+  const filteredReactions = reactions.filter(
+    (reaction) =>
+      reaction.symbol !== "👍" &&
+      reaction.symbol !== "❤️" &&
+      reaction.symbol !== "😂"
+  );
+
+  const restOfTheReactions = filteredReactions.map((reaction) => {
+    const dropdownItem = document.createElement("li");
+    dropdownItem.classList.add("dropdown-item");
+    const button = document.createElement("button");
+    button.classList.add("btn");
+    button.innerHTML = `
+      <span class="ms-2">${reaction.symbol}</span>
+      <span class="ms-2">${reaction.count}</span>
+    `;
+    dropdownItem.append(button);
+    return dropdownItem;
+  });
+
+  const dropdownMenu = container.querySelector(".dropdown-menu");
+  dropdownMenu.append(...restOfTheReactions);
+
+  const buttons = container.querySelectorAll(".react-button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      reactionHandler(postId, button.children[0].textContent, (reactions) => {
+        console.log(reactions);
+      });
+    });
+  });
+
   return container;
 }
