bde3e521769a3c07b0c7d190aefb84c9a1d35459
Kim Rune Møller <kimrune@hotmail.no>
2023-10-11 00:04:18 +0200

    fix profile page js script

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -1,5 +1,5 @@
-import { usersPosts } from "../utils/API/api.js";
-import "../components/post/post.mjs";
+import { usersPosts, profile } from "../utils/API/api.js";
+import postComponent from "../components/posts/post.mjs";
 
 export function main() {
   const userData = JSON.parse(localStorage.getItem("userData"));
@@ -10,12 +10,44 @@ export function main() {
     username = userData.name;
   }
 
-  usersPosts(username, userData).then((data) => {
-    const postsElement = document.querySelector("#posts");
-    data.forEach((post) => {
-      const postElement = document.createElement("post-component");
-      postElement.setAttribute("props", JSON.stringify(post));
-      postsElement.appendChild(postElement);
+  profile(username, userData).then((profileData) => {
+    console.log(profileData);
+    const bannerElement = document.querySelector("#profile-banner");
+    bannerElement.addEventListener("load", () => {
+      bannerElement.classList.add("fade-in");
+    });
+    bannerElement.addEventListener("error", () => {
+      bannerElement.src = "../../assets/images/banner_placeholder.png";
+    });
+    bannerElement.src = `${profileData.banner}`;
+
+    const profileNameElement = document.querySelector("#profile-name");
+    profileNameElement.textContent = profileData.name;
+    profileNameElement.classList.add("bounce-in");
+
+    const profilePictureElement = document.querySelector("#profile-picture");
+    profilePictureElement.src = `${profileData.avatar}`;
+    profilePictureElement.addEventListener("load", () => {
+      profilePictureElement.classList.add("fade-in");
+    });
+    profilePictureElement.addEventListener("error", () => {
+      profilePictureElement.src = `https://ui-avatars.com/api/?name=${username.substring(
+        0,
+        5
+      )}&background=random&size=64`;
+    });
+
+    const profilePictureNameElement = document.querySelector(
+      "#profile-picture-name"
+    );
+    profilePictureNameElement.textContent = profileData.name;
+  });
+
+  const postsContainer = document.querySelector("#posts-container");
+  usersPosts(username, userData).then((posts) => {
+    posts.forEach((post) => {
+      const postElement = postComponent(post.id, post, false);
+      postsContainer.append(postElement);
     });
   });
 }
