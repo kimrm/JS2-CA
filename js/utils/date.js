0816fa787102f9b54dc459fb2917acde7f83b1a1
Kim Rune Møller <kimrune@hotmail.no>
2023-10-13 00:56:58 +0200

    shortens the time ago string

--- js/utils/date.js
+++ js/utils/date.js
@@ -4,15 +4,15 @@ export function timeAgo(timestamp) {
   const timeDifferenceInSeconds = Math.floor((timeNow - timePosted) / 1000);
 
   if (timeDifferenceInSeconds < 60) {
-    return `${timeDifferenceInSeconds} second${
+    return `${timeDifferenceInSeconds} sec${
       timeDifferenceInSeconds !== 1 ? "s" : ""
     } ago`;
   } else if (timeDifferenceInSeconds < 3600) {
     const minutes = Math.floor(timeDifferenceInSeconds / 60);
-    return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+    return `${minutes} min${minutes !== 1 ? "s" : ""} ago`;
   } else if (timeDifferenceInSeconds < 86400) {
     const hours = Math.floor(timeDifferenceInSeconds / 3600);
-    return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+    return `${hours} hr${hours !== 1 ? "s" : ""} ago`;
   } else {
     const days = Math.floor(timeDifferenceInSeconds / 86400);
     return `${days} day${days !== 1 ? "s" : ""} ago`;
